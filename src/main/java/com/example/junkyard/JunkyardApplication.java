804ec83d0e26051b12cdfe5e156665ccc34ace5e
arcadeVault <neoarcadegame0@gmail.com>
2025-02-15 12:08:02 +0530

    added base changes

---
+++ src/main/java/com/example/junkyard/JunkyardApplication.java
@@ -0,0 +1,13 @@
+package com.example.junkyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunkyardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunkyardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/junkyard/JunkyardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junkyard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunkyardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
